059ba074bda35c605063b0bef49b4e69c002556a
hettling <hannes.hettling@gmail.com>
2016-08-02 15:50:05 +0200

    adding index

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Type" content="cache" />
+    <meta name="robots" content="INDEX, FOLLOW" />
+    <meta name="keywords" content="THE ANTONELLI LAB – evolution and biogeography with focus on the Neotropics" />
+    <meta name="description" content="THE ANTONELLI LAB – evolution and biogeography with focus on the Neotropics" />
+	<link href="styles.css" rel="stylesheet" type="text/css" media="screen" />
+    <link rel="shortcut icon" href="http://www.antonelli-lab.net/favicon.ico" />
+	<title>THE ANTONELLI LAB – evolution and biogeography with focus on the Neotropics</title>
+<!--GOOGLE ANALYTICS and button function SCRIPT-->
+<script type="text/javascript">
+var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-29881380-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+function MM_swapImgRestore() { //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+</script>
+</head>
+<!--BODY STARTS HERE-->
+<body onload="MM_preloadImages('images/knappar/people.png','images/knappar/projects.png','images/knappar/publications.png','images/knappar/photos.png','images/knappar/resources.png', 'images/knappar/joinus.png')">
+<div id="main">
+<div id="wrap">
+<!--compact top-->
+<div id="compact_top">
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+
+
+<div id="top_logo"> <a href="index.php"><img src="images/logo.png" alt="top logo" width="473" height="78" border="0" /></a></div>
+
+<ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="people.php">People</a></li>
+    <li><a href="projects.php">Projects</a></li>
+    <li><a href="publications.php">Publications</a></li>
+    <li><a href="photos.php">Photos</a></li>
+    <li><a href="resources.php">Resources</a></li>
+    <li><a href="join.php">Join us</a></li>            
+    <li><a href="wiki">Member's Wiki</a></li>
+</ul>
+    
+<div class="stripe">
+     <img src="images/stripe.png" width="950" height="2" />
+</div>
+</div>
+<!--banner-->
+  <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="950" height="127" id="FlashID2" title="banner">
+    <param name="movie" value="sliding banner/sliding_banner.swf" />
+    <param name="quality" value="high" />
+    <param name="wmode" value="opaque" />
+    <param name="swfversion" value="6.0.65.0" />
+    <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+    <param name="expressinstall" value="Scripts/expressInstall.swf" />
+    <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+    <!--[if !IE]>-->
+    <object type="application/x-shockwave-flash" data="sliding banner/sliding_banner.swf" width="950" height="127">
+      <!--<![endif]-->
+      <param name="quality" value="high" />
+      <param name="wmode" value="opaque" />
+      <param name="swfversion" value="6.0.65.0" />
+      <param name="expressinstall" value="Scripts/expressInstall.swf" />
+      <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+      <div>
+        <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+        <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+      </div>
+      <!--[if !IE]>-->
+    </object>
+    <!--<![endif]-->
+  </object>
+<!--END banner-->
+<!--compact content area-->
+<div id="content1">
+
+<!--ALEX ALEX ALEX ALEX ALEX ALEX-->
+
+<div id="content_home">
+<p>We want to understand how biological diversity has evolved and how it will be affected by on-going climate change and habitat destruction. The scope of our studies range from specific organism groups in isolated regions in the Neotropics to global cross-taxonomic patterns.</p>
+<p>We attempt to integrate data and methodologies from molecular phylogenetics, palaeobiology, biogeography, climatic modelling, bioinformatics and ecology. Our work has shed further light on the evolution of several taxa and biomes but a lot more remains to be done. <a href="projects.php">Read more</a></p>
+</div>
+
+<div id="twitter_box">
+<h2 class='title'>@antonelli_lab</h2>
+<a class="twitter-timeline" href="https://twitter.com/antonelli_lab" data-widget-id="579696233171824640">Tweets by @antonelli_lab</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
+
+
+<div id="buttons">
+    <a href="people.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image5','','images/knappar/people.png',1)"><img src="images/knappar/people_shadow.png" name="Image5" width="140" height="91" border="0" id="Image5" /></a>
+    <a href="projects.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image6','','images/knappar/projects.png',1)"><img src="images/knappar/projects_shadow.png" name="Image6" width="140" height="91" border="0" id="Image6" /></a>
+    <a href="publications.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image7','','images/knappar/publications.png',1)"><img src="images/knappar/publications_shadow.png" name="Image7" width="140" height="91" border="0" id="Image7" /></a>
+    <a href="photos.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image9','','images/knappar/photos.png',1)"><img src="images/knappar/photos_shadow.png" name="Image9" width="140" height="91" border="0" id="Image9" /></a>
+    <a href="resources.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image10','','images/knappar/resources.png',1)"><img src="images/knappar/resources_shadow.png" name="Image10" width="140" height="91" border="0" id="Image10" /></a>
+    <a href="join.php" onmouseout="MM_swapImgRestore()" onmouseover="MM_swapImage('Image11','','images/knappar/joinus.png',1)"><img src="images/knappar/joinus_shadow.png" name="Image11" width="140" height="91" border="0" id="Image11" /></a>
+</div><!--END buttons-->
+</div><!--END compact content-->
+</div><!--END wrap-->
+</div><!--END main-->
+<!--footer-->
+<div id="footer">
+<div id="foot">
+
+<div class="stripe">
+  <img src="images/stripe.png" width="950" height="2" /></div>
+  
+<div id="note">
+
+</div>
+
+<p>University of Gothenburg - Department of Biological and Environmental Sciences<br />
+
+Carl Skottsbergs gata 22B - P.O. Box 461 - SE 405 30 - Göteborg - SWEDEN</p>
+
+<p>Contact: Alexandre Antonelli - E-mail: <a href="mailto:&#97;&#108;&#101;&#120;&#97;&#110;&#100;&#114;&#101;&#46;&#97;&#110;&#116;&#111;&#110;&#101;&#108;&#108;&#105;&#64;&#103;&#117;&#46;&#115;&#101;">&#97;&#108;&#101;&#120;&#97;&#110;&#100;&#114;&#101;&#46;&#97;&#110;&#116;&#111;&#110;&#101;&#108;&#108;&#105;&#64;&#103;&#117;&#46;&#115;&#101;</a> - Phone: +46 703 989570</p>
+
+<div id="darlingsniper">
+<p>Site by <a href="http://www.arken25.se" target="_blank">Arken25.se</a></p>
+</div>
+
+</div><!--END foot--></div><!--END footer-->
+<script type="text/javascript">
+swfobject.registerObject("FlashID2");
+</script>
+</body>
+</html>
