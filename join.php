c3eebd269b2ab0f6677804a616561dbec1d822d3
Mats Töpel <mats@topel.se>
2016-08-15 16:26:16 +0200

    Removed the 'About us' part of the text

--- join.php
+++ join.php
@@ -69,63 +69,6 @@
 
 <div class="hr"></div>
 
-<div id="content_resources">
-
-<br>
-<div class="header_4">OUR VISION</div> 
-<br>
-<br>
- 
-<p><i>To be one of the most <b>creative, productive </b>and <b>exciting </b>research groups in evolutionary biology and biogeography. </i></p>
-
-<p>In other words:<i> We aim to do <b>great science </b>and have <b>a lot of fun</b>!</i></p>
-<br>
-
-<div class="hr_4"></div>
-<div class="header_4">CORE VALUES</div> 
-<br>
-<br>
-<br>
-<p><b>Quality.</b> We prioritize quality above quantity in scientific publications, by investing on generating large data sets and performing robust analyses to tackle outstanding problems. We often target inter-disciplinary, and whenever possible, highly ranked scientific journals.</p>
-
-<p><b>Innovation.</b> We develop new methodological solutions, challenge old assumptions, test competing hypotheses, and experiment with new techniques. We brainstorm on each research project and design papers on an individual basis. </p>
-
-<p><b>Openness.</b> We share data, ideas, unpublished manuscripts and research applications within the group to foster cross-pollination of ideas and increase internal collaborations. At the same time, we take intellectual property and personal integrity seriously. </p>
-
-<p><b>Commitment. </b>We love what we do and take full responsibility for our commitments, working actively and enthusiastically to achieve our goals and advance our science.</p>
-<br>
-<div class="hr_4"></div>
-<div class="header_4">THE ANTONELLI LAB IN A NUTSHELL</div> 
-<br>
-<br>
-<p><b>Who are we?</b></p>
-
-<p>We are a research group comprising different <i>academic and technical categories</i> (associate professors, post-docs, students at PhD, MSc and BSc levels, bioinformaticians, laboratory technicians, research assistants) with <i>various scientific backgrounds</i> (biology, ecology, computational biology, geology, botany, zoology). We represent many <i>nationalities</i> (Brazilian, German, Dutch, Italian, American, French, Bolivian, Colombian, Swedish, Thai, Spanish, Finnish), work with many different <i>scientific questions</i> (in molecular phylogenetics, macroevolution, biogeography, population genetics, climate change, taxonomy, palaeontology), <i>organism groups</i> (plants, birds, frogs, insects, mammals, snakes, microorganisms) and <i>approaches</i> (empirical, theoretical and methodological). </p>
-<br>
-<p><b>What unifies our group?</b></p>
-
-<p>Despite our breadth of backgrounds and interests, we are united by a genuine interest in understanding <b>how biological diversity has evolved, how it varies over space and time, </b>and<b> which factors and processes determine this variation</b>. </p>
-<br>
-<p><b>How are we structured?</b></p>
-
-<p>We consist of two main categories: <i>core participants</i> (those funded through the PI's grant at the <a href="http://www.gu.se">University of Gothenburg</a>, or [for MSc students] having the university as their primary institution) and <i>associated researchers and students </i>(those actively collaborating with the group but employed or registered elsewhere). Associated members spend at least some time at the <a href="http://www.bioenv.gu.se">Department of Biological and Environmental Sciences</a> (located within the <a href="http://www.gotbot.se">Gothenburg botanical garden</a>) through internships or research visits. </p>
-<br>
-
-<div class="hr_4"></div>
-<div class="header_4">AVAILABLE RESOURCES</div> 
-<br>
-<br>
-<p>Most resources are shared and subject to common rules and praxis. These include, among others:<br />
-<br />
-<b>State-of-the-art DNA labs and knowledge.</b> We have access to several rooms for DNA and RNA extraction, quantification (through electrophoresis, TapeStation and Nanodrop machines), amplification and PCR. We are moving away from Sanger DNA sequencing (whereby only a few genetic markers are sequenced) to next-generation sequencing (NGS) techniques, resulting in vast amounts of genetic data at relatively low costs (after suitable protocols and probes have been designed and tested, which are often taxon-specific). We are performing sequence capture (targeting genes using taxon-specific probes), generating transcriptomes, ultra-conservative elements (UCEs), and full genomes. We have extensive in-house experience with library preparation, barcoding and multiplexing, and hybrid gene capture. These resources are part of the <a href="http://http://bioenv.gu.se/english/research/research-resources/nosi">Non-model Organism Sequencing Infrastructure (NOSI)</a> at our department.<br />
-<br />
-<b>Genomic infrastructures.</b> We use the two national genomic facilities, <a href="http://http://www.scilifelab.se/">SciLifeLab</a> (Stockholm &amp; Uppsala) and the <a href="http://http://cf.gu.se/english/genomics">Sahlgrenska Academy Core Facility</a> (Gothenburg) for genomic sonication and sequencing (mostly utilising the Illumina MySeq and HiSeq platforms). At SciLifeLab we have the advantage of not having to pay for labour (whose costs are covered by the Swedish Research Council, and regularly amount to c. 40% of total sequencing costs). <br />
-<br />
-<b>Computational resources.</b> We use our own computers to design and set up analyses, but then run heavier computational tasks on external resources, selected in relation to the specific needs. First off, we have a shared MacPro (3.5 GHz 6-Core Intel Xeon, 32 GB memory) called spacemule. For more demanding tasks, we use <a href="http://http://www.matstopel.se/albiorix">Albiorix</a>, which is a set of interconnected computers (c. 100 nodes) dedicated to high-performance bioscience computing physically located at our department. For more complex tasks we have accounts at the <a href="http://http://www.snic.se/">Swedish National Infrastructure for Computing</a>. We have access to bioinformatic expertise both within the group (planned to increase in January 2016 through the hiring of a bioinformatician working 50% for us) and through the Swedish national <a href="http://http://bils.se/">Bioinformatic Infrastructure for Life Sciences</a>. We are also working towards increasing bioinformatic knowledge among group members through on-demand workshops and mini-courses (e.g. in Unix, databases, R, genomic annotation). <br />
-<br />
-<b>Natural history and living collections.</b> Our department hosts the <a href="http://http://bioenv.gu.se/english/research/research-resources/Herbarium/%3FlanguageId=100001%26disableRedirect=true%26returnUrl=http%253A%252F%252Fbioenv.gu.se%252Fforskning%252Fforskningsresurser%252Fherbarium%252F">Herbarium GB</a>, housing c. 1 million dried plant specimens. A large proportion of these collections were made in South America, through the work of dozens of researchers and students, often in connection with the long-standing <a href="http://http://www2.dpes.gu.se/project/ecuador/">Flora of Ecuador</a> project coordinated from our department. <a href="http://www.gotbot.se">Gothenburg botanical garden</a> is one of the largest botanical gardens in Europe with an area of 175 hectares, where about 15000 species and cultivars are grown and of free access to researchers. The <a href="http://http://www.goteborg.com/en/goteborgs-naturhistoriska-museum/">Natural History Museum in Gothenburg</a> and the <a href="http://http://www.nrm.se/">Swedish Museum of Natural History</a> in Stockholm house together tens of millions of animal and plant specimens that are often used by members in our group for taxonomic purposes and as genetic resources. We also have extensive collaboration with many other natural history and living collections around Europe and worldwide. </p>
-<br>
-<div class="hr_4"></div>
 <div class="header_4">INTERESTED IN JOINING OUR GROUP?</div> 
 <br>
 <br>
@@ -134,38 +77,6 @@
 <p>A pre-condition for joining the group is that you are aware of, and fully agree with, the way we work and the commitment we expect from all members of the lab. You will therefore be asked to read and sign our <a href="pdf/Working_agreement.doc"<i>working agreement</i></a>. </p>
 
 <p>If you feel qualified and agree with the above, do get in touch to discuss potential collaborations and a possible affiliation to our group! You will be asked to provide a <a href="https://docs.google.com/document/d/1rFbFpJzvZ0X4YTsahKM_0Txsi5zOqxQKPj2aC_KY9Ho/edit?usp=sharing"<i>one-page outline</i></a> of your main research questions and how you plan to develop them. Please send this together with your CV to <a href="mailto:alexandre.antonelli@bioenv.gu.se">Alexandre Antonelli</a>.</p>
-
-<div class="hr_4"></div>
-<div class="header_4">*** FULLY FUNDED POSITIONS CURRENTLY AVAILABLE ***</div> 
-<br>
-<br>
-
-<p>Here we list any current openings to our lab. All openings are advertised in <a href="http://life.mcmaster.ca/evoldir.html">EvolDir</a> and sent out to our <a href="https://twitter.com/antonelli_lab">Twitter followers</a>, among other media. 
- </p>
-
-<p><strong>There are no open positions at the moment. (The Assistant Professorships and PhD positions advertised earlier this year are currently under evaluation)</strong> 
-
-<div class="hr_4"></div>
-<div class="header_4">VOICES FROM THE LAB</div> 
-<br>
-<br>
-<p><i>"Working in the Antonelli group has changed my way of working, the level of collaboration we encourage moves projects along faster and promotes better research, advancing my career in many ways. I also consistently feel important and valuable to the group, creating an enriching environment for my development and success."</i> &ndash; Christine Bacon</p>
-
-<p><i>"As a masters graduate, working in the Antonelli group gave me the opportunity to get involved in a large number of projects early in my career. Having the freedom to explore interests with the support of a multidisciplinary community has been a great experience."  </i> &ndash; Ruud Scharn</p>
-
-<p><i>"We think and work as a team; I take part in great discussions every day with my colleagues, improving biological and evolutionary concepts and my English. Here to think about science is light and fun."  </i> &ndash; Thais Guedes</p>
-
-<p><i>"The Antonelli lab has been giving me freedom and inspiration to develop new ideas and the resources and scientific input to concretize them. Collaborative effort and enthusiasm from the group are essential elements in all projects I have worked on here." </i> &ndash; Daniele Silvestro</p>
-
-<p><i>"In the Antonelli group I have the opportunity to collaborate with great people who give me theoretical and practical support to explore innovative ideas. The multidisciplinary view of the group helped me to open my own mind about biodiversity as a whole." </i> &ndash; Fernanda Carvalho</p>
-
-<p><i>"I experience this as a very dynamic, future-oriented and welcoming group. It gave me an invaluable opportunity to carry out my Master’s project, providing me with great collaborative skills, advanced knowledge of state-of-the-art computational methods and a general scientific confidence, which reaches far beyond my actual project." </i> &ndash; Tobias Hofmann</p>
-
-<p><i>"As an associated doctoral student to the Antonelli group, I was connected to a group of international and interdisciplinary researchers with disparate, but complementary, academic interests and experiences. These connections resulted in many active collaborations, more than one peer-reviewed paper during my Ph.D. research, invitations to ongoing and future fieldwork, an understanding of academia outside of the US, and many friendships. Continued access to this community, and particularly the careful, positive mentorship that Alex provides as the group leader, is a large reason why I decided to continue working with the group as postdoctoral researcher." </i> &ndash; Laura Lagomarsino</p>
-
-<p><i>“Productive and inclusive atmosphere. Nice and open people. Excellent publications. International network. Freedom to pursue own ideas. Great courses from all Nordic universities. Good opportunities to participate in international conferences and workshops. I couldn’t think of a better place for my PhD studies.” </i> &ndash; Alexander Zizka</p>
-
-
 
 
   
